e5c454553988aa156d04d77f349537f6471f3a7a
Nitin Ashok Pawar <npawar@altimetrik.com>
2019-08-07 14:29:17 +0530

    adds microservies

---
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,47 @@
+package com.example.currencyconversionservice;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+
+    @Bean
+    RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @Autowired
+    CurrencyExchnageServiceProxy currencyExchnageServiceProxy;
+
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    @HystrixCommand(fallbackMethod = "convertCurrencyFallback")
+    public CurrenyConversionBean convertCurrency(@PathVariable String from,@PathVariable String to,@PathVariable int quantity){
+
+        String url = loadBalancerClient.choose("currency-exchange-service").getUri().toString();
+        System.out.println("server is:"+url);
+        ExchangeValue exchangeValue =  getRestTemplate().getForObject(url+"/currency-exchange/from/"+from+"/to/"+to,ExchangeValue.class);
+
+        return new CurrenyConversionBean(1,from,to,exchangeValue.getConversionMultiple(),quantity,exchangeValue.getConversionMultiple()*quantity);
+    }
+
+    public CurrenyConversionBean convertCurrencyFallback( String from,String to,int quantity){
+        return new CurrenyConversionBean();
+    }
+    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrenyConversionBean convertCurrencyFeign(@PathVariable String from,@PathVariable String to,@PathVariable int quantity){
+
+        ExchangeValue exchangeValue =  currencyExchnageServiceProxy.retriveExchnageValue(from,to);
+        return new CurrenyConversionBean(1,from,to,exchangeValue.getConversionMultiple(),quantity,exchangeValue.getConversionMultiple()*quantity);
+    }
+}
---
+++ currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,21 @@
+package com.example.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.example")
+@RibbonClient(name="currency-exchange-service")
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
